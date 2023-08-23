63aca118f5853b4bb5fe1cac0c0c4c67b8cb2c20
Jorge Aguilar <jorge.agvel@gmail.com>
2023-08-22 20:45:10 -0600

    feature(useEffect - useRef)

--- mi-app/src/App.tsx
+++ mi-app/src/App.tsx
@@ -1,15 +1,20 @@
 import { Counter } from "./components/Counter";
+import { TimerPadre } from "./components/TimerPadre";
 import { Usuario } from "./components/Usuario";
 
 function App() {
-    return (
-        <>
-            <h1>React + TypeScript</h1>
-            <hr />
-            <Counter />
-            <Usuario />
-        </>
-    );
+  return (
+    <>
+      <h1>React + TypeScript</h1>
+      <hr />
+      <h2>useState</h2>
+      <Counter />
+      <Usuario />
+      <hr />
+      <h2>useEffect - useRef</h2>
+      <TimerPadre />
+    </>
+  );
 }
 
 export default App;
--- mi-app/src/components/Usuario.tsx
+++ mi-app/src/components/Usuario.tsx
@@ -1,32 +1,31 @@
 import { useState } from "react";
 
 interface User {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
 }
 
 const tempUser: User = {
-    id: 666,
-    name: "JA",
+  id: 666,
+  name: "JA",
 };
 
 export const Usuario = () => {
-    const [user, setUser] = useState<User>(tempUser);
+  const [user, setUser] = useState<User>(tempUser);
 
-    const login = () => {
-        setUser({
-            id: 123,
-            name: "Jorge",
-        });
-    };
+  const login = () => {
+    setUser({
+      id: 123,
+      name: "Jorge",
+    });
+  };
 
-    return (
-        <div className="mt-5">
-            <h3>Usuario: useState</h3>
-            <button onClick={login} className="btn btn-outline-primary">
-                Login
-            </button>
-            {!user ? <pre>No hay usuario</pre> : <pre>{JSON.stringify(user)}</pre>}
-        </div>
-    );
+  return (
+    <div className="my-4">
+      {!user ? <pre>No hay usuario</pre> : <pre>{JSON.stringify(user)}</pre>}
+      <button onClick={login} className="btn btn-outline-primary mt-2">
+        Login
+      </button>
+    </div>
+  );
 };
