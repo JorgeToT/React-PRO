6a4e20ce9471b3d1fd8666d805c0c6e2cea983ca
Jorge Aguilar <jorge.aguilar@gluo.mx>
2024-02-10 20:41:40 -0600

    feature(nested lazy routes)

--- 02-react-app/src/01-lazyload/layout/LazyLayout.tsx
+++ 02-react-app/src/01-lazyload/layout/LazyLayout.tsx
@@ -1,11 +1,31 @@
+import { NavLink, Routes, Route, Navigate } from "react-router-dom";
+import { LazyPage1, LazyPage2, LazyPage3 } from "../pages";
 export const LazyLayout = () => {
     return (
         <div>
             <h1>Lazy Layout</h1>
-            <p>
-                This layout is lazy-loaded. It is not loaded until the user
-                navigates to a route that uses it.
-            </p>
+            <ul>
+                <li>
+                    <NavLink to="lazy1">Page 1</NavLink>
+                </li>
+                <li>
+                    <NavLink to="lazy2">Page 2</NavLink>
+                </li>
+                <li>
+                    <NavLink to="lazy3">Page 1</NavLink>
+                </li>
+            </ul>
+
+            <Routes>
+                <Route path="lazy1" element={<LazyPage1 />} />
+                <Route path="lazy2" element={<LazyPage2 />} />
+                <Route path="lazy3" element={<LazyPage3 />} />
+
+                {/* <Route path="*" element={<h1>Choose a page</h1>} /> */}
+                <Route path="*" element={<Navigate replace to="lazy1" />} />
+            </Routes>
         </div>
     );
 };
+
+export default LazyLayout;
